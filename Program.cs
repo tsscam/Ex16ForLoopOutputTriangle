08382030a2ac977d0634fc9aeb63c30dc8c8f6cd
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-20 09:38:22 -0400

    original commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+                //Call Methods
+                stars(5);
+            }
+        
+            //method
+            public static void stars(int n)
+            {
+                for (int i = 5; i <= 5; i++)
+                {
+                    Console.WriteLine(new String('*', i));
+                    Console.WriteLine(new String('*', i));
+                    Console.WriteLine(new String('*', i));
+                    Console.WriteLine(new String('*', i));
+                    Console.WriteLine(new String('*', i));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
